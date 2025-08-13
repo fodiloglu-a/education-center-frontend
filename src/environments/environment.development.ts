906133cbcc995fa85909dc901c7d8c914969b7b0
fodiloglu-a <mutalip.fodiloglu@gmail.com>
2025-08-14 00:49:00 +0300

    initial commit

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@
 // 'ng serve' veya 'ng build' (varsayılan olarak) komutlarıyla derlendiğinde kullanılır.
 // Üretim ortamı için 'environment.ts' dosyasına bakın.
 export const environment = {
-  production: false, // Geliştirme ortamı olduğunu belirtir
-  apiUrl: 'http://localhost:8080/api' // Backend API'sinin geliştirme URL'si
+  production: true, // Geliştirme ortamı olduğunu belirtir
+  apiUrl: 'https://education-center-backand.onrender.com' // Backend API'sinin geliştirme URL'si
 };
 
