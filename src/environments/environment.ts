8ff72ab0a65376df23e0ae27053f8b7149deae99
fodiloglu-a <mutalip.fodiloglu@gmail.com>
2025-10-08 15:19:31 +0300

    payment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@
 // 'ng build --configuration=production' veya 'ng build' komutlarıyla derlendiğinde kullanılır.
 export const environment = {
   production: true, // Üretim ortamı olduğunu belirtir
-    // apiUrl: 'https://education-center-backand.onrender.com/api'
-    apiUrl: 'http://localhost:8080/api'
+   apiUrl: 'https://education-center-backand.onrender.com/api'
+  // apiUrl: 'http://localhost:8080/api'
 };
